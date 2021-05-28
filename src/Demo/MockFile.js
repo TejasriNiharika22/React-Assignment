556f244d73a459b7d5cc597a31b97b2fb846d4df
TejasriNiharika22 <84013826+TejasriNiharika22@users.noreply.github.com>
2021-05-28 11:31:09 +0530

    Add files via upload

---
+++ src/Demo/MockFile.js
@@ -0,0 +1,12 @@
+const Demo={
+    text: "What's the plan for today?",
+    sub: "Todo-List",
+    addButton: "AddTask",
+    editButton: "Edit",
+    delButton: "Delete",
+    modalPopup: "Edit Task",
+    placeholder: "Enter your task ",
+    cancelButton: "Close",
+    saveButton: "Save",
+}
+export default Demo;
