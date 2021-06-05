f7563ca00d3fe590d63ce0f7aa8653f574157e5a
TejasriNiharika22 <84013826+TejasriNiharika22@users.noreply.github.com>
2021-06-05 12:24:40 +0530

    Add files via upload

---
+++ src/mock/MockData.js
@@ -0,0 +1,12 @@
+const data={
+    text: "What's the plan for today?",
+    sub: "Todo-List",
+    addButton: "AddTask",
+    editButton: "Edit",
+    delButton: "Delete",
+    modalPopup: "Edit Task",
+    placeholder: "Enter your task ",
+    cancelButton: "Close",
+    saveButton: "Save",
+}
+export default data;
